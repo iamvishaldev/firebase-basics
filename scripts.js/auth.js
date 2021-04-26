82845952834f7801f97ea1efc95e4653d36d064e
Vishal Yadav <yvishal706@gmail.com>
2021-04-26 19:59:58 +0530

    Login update

--- scripts.js/auth.js
+++ scripts.js/auth.js
@@ -1,3 +1,6 @@
+const myModel = document.querySelectorAll(".modal");
+
+// Signup
 async function signup(e) {
   e.preventDefault();
   const email = document.querySelector("#signupEmail");
@@ -14,4 +17,30 @@ async function signup(e) {
     console.log(err);
     M.toast({ html: err.message, classes: "red" });
   }
+  email.value = "";
+  password.value = "";
+  M.Modal.getInstance(myModel[0]).close();
+}
+
+// Login
+
+async function Login(e) {
+  e.preventDefault();
+  const email = document.querySelector("#loginEmail");
+  const password = document.querySelector("#loginPassword");
+  console.log(email.value, password.value);
+
+  try {
+    const result = await firebase
+      .auth()
+      .signInWithEmailAndPassword(email.value, password.value);
+    console.log(result);
+    M.toast({ html: `Welcome ${result.user.email}`, classes: "green" });
+  } catch (err) {
+    console.log(err);
+    M.toast({ html: err.message, classes: "red" });
+  }
+  email.value = "";
+  password.value = "";
+  M.Modal.getInstance(myModel[1]).close();
 }
